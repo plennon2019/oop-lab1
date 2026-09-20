8e9d871fce672e996d3fac240df4b10393f771aa
Paul Lennon <Paul.Lennon@atu.ie>
2026-09-20 10:44:38 +0100

    Added displayDetails method to Book

--- src/main/java/ie/atu/oop/week1/Book.java
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -6,4 +6,13 @@ public class Book {
     public String author;
     public int pageCount;
     public boolean available = true;
+
+    public void displayDetails()
+    {
+        System.out.println("Book title: " + title);
+        System.out.println("Book author: " + author);
+        System.out.println("Book page count: " + pageCount);
+        System.out.println("Book available: " + available);
+    }
+
 }
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -2,6 +2,12 @@ package ie.atu.oop.week1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Book firstbook = new Book();
+        firstbook.title = "Dune";
+        firstbook.author = "Frank Herbert";
+        firstbook.pageCount = 412;
+
+       firstbook.displayDetails();
     }
 }
